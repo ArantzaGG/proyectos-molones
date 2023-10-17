fea8d2244873910b18e94ff15485e8a481803851
BarbaraBravoRedondo <barbarabravoredondo@gmail.com>
2023-10-17 19:42:39 +0200

    props and functions have been fix to the each folder

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -17,40 +17,14 @@ function App() {
     nameAuthor: '',
     job: '',
   });
+  const updateInput = (data) => {
+    setData(data);
+  };
   
-  const handleInput = (ev) => {
-    const id = ev.target.id;
-    const value = ev.target.value;
-    setData({ ...data, [id]: value });
+  const updateCard = (error) => {
+    setError(error);
   };
-
-  const handleCard = () => {
-    const regex = /^(https?|ftp):\/\/[^\s/$.?#].[^\s]*$/i;
-
-    if (data.nameProject === '') {
-      setError('El campo del nombre del proyecto es obligatorio');
-    } else if (data.slogan === '') {
-      setError('El campo slogan es obligatorio');
-    } else if (data.repo !== '' && !regex.test(data.repo)) {
-      setError('La URl del campo repo no es válida');
-    } else if (data.repo === '') {
-      setError('El campo repo es obligatorio');
-    } else if (data.demo !== '' && !regex.test(data.demo)) {
-      setError('La URl del campo demo no es válida');
-    } else if (data.demo === '') {
-      setError('El campo demo es obligatorio');
-    } else if (data.technologies === '') {
-      setError('El campo de las tecnologias es obligatorio');
-    } else if (data.desc === '') {
-      setError('El campo de la descripcion es obligatorio');
-    } else if (data.nameAuthor === '') {
-      setError('El campo del autor es obligatorio');
-    } else if (data.job === '') {
-      setError('El campo del trabajo es obligatorio');
-    } else {
-      setError('');
-    }
-  };
+  
 
   return (
     <div className="container">
@@ -58,7 +32,7 @@ function App() {
       <div className='div__MQ'>
       <main className="main">
         <CardPreview data={data}/>
-        <Form data={data}/>
+        <Form data={data} updateInput={updateInput} updateCard={updateCard} error={error}/>
       </main>
       </div>
     </div>
