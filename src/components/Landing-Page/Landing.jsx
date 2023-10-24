02b0cc946ee3afee7780bdd93c056547ecc737bb
JohnathanEsguerra <darling.rayranc@gmail.com>
2023-10-24 13:12:55 +0200

    Add basic landing

--- src/components/Landing-Page/Landing.jsx
+++ src/components/Landing-Page/Landing.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import '../styles/App.scss';
+import Logo from '../../images/proyectos_molones.png';
+
 
 function Landing() {
   return (
-    <div>Landing</div>
+    <div className='landing'>
+      <img className='Logo' src={Logo} alt="logo"/>
+      <h1>Desarrolladoras web</h1>
+      <h2>¿Quieres solicitar nuestros servicios?</h2>
+      <p>¡Haz llegado a la web correcta!</p>
+      <button className="btn-Start">Comienza Ahora</button>
+    </div>
   )
 }
 
-export default Landing
+export default Landing;
