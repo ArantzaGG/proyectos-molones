a6c5c494dcb8fa47a3ffc3344c4b3178781e1f78
ArantzaGG <arantzagomez@hotmail.es>
2023-10-27 13:12:14 +0200

    add link to landing in header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,16 @@
-import logo from '../../images/solo-logo.png';
-import { Link } from 'react-router-dom';
-import '../../styles/Header.scss';
+import logo from "../../images/solo-logo.png";
+import { Link } from "react-router-dom";
+import "../../styles/Header.scss";
 
 const Header = () => {
   return (
     <div>
-      <header className='header'>
-        <img src={logo} alt='logo' className='header__logo' />
-        <p className='header__text'>Proyectos Molones</p>
+      <header className="header">
+        <Link to="/">
+          <img src={logo} alt="logo" className="header__logo" />
+        </Link>
+        <p className="header__text">Proyectos Molones</p>
       </header>
-      
     </div>
   );
 };
